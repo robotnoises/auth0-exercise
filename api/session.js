89a5d2b33d4b3cdbee349f166d88bcb4d2bda026
David Nichols <davenich@gmail.com>
2016-08-29 21:02:37 -0400

    Add a callback route  for Auth0

--- api/session.js
+++ api/session.js
@@ -1,8 +1,16 @@
-var express = require('express');
+var passport = require('passport');
 
-module.exports = function (router) {
-  // Temp
-  router.get('/ping', (req, res) => {
-    res.send('hi.');
-  });
+module.exports = (router) => {
+  
+  // /callback => Handle Auth0 new session callback
+  router.get('/callback', 
+    passport.authenticate('auth0', { failureRedirect: '/client/#/session' }),
+    (req, res)  => {
+      if (!req.user) {
+        throw new Error('user null');
+      }
+      res.redirect("/user");
+    });
 };
+
+
