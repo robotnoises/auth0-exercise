0120e765e497ea8652b1f37f3bee56695d064527
David Nichols <davenich@gmail.com>
2016-09-13 22:06:09 -0400

    Add greeting messages to the banner

--- app/globals/directives/bannerDirective.js
+++ app/globals/directives/bannerDirective.js
@@ -17,6 +17,9 @@
     return {
       restrict: 'E',
       replace: true,
+      scope: {
+        admin: '=admin'
+      },
       template: 
         '<div class="banner" ng-hide="dismissed">' +
         '  <i class="fa fa-times dismiss" ng-click="dismiss()"></i>' +
@@ -36,6 +39,26 @@
           scope.notifications.push(message);
           scope.dismissed = false;
         });
+
+        // Greetings
+        function loadGreetings() {
+          var userGreeting = 'Welcome! Since you\'re just a lowly User, all you can do is view your own profile. Sorry.';
+          var adminGreeting = 'Almighty Admin, you may choose any profile to view or edit.';
+          var userGreeted = localStorage.getItem('userGreeted');
+          var adminGreeted = localStorage.getItem('adminGreeted');
+
+          if (!scope.admin && !userGreeted) {
+            localStorage.setItem('userGreeted', 'true');
+            scope.notifications.push({ message: userGreeting, type: 'info' });
+            scope.dismissed = false;
+          } else if (scope.admin && !adminGreeted) {
+            localStorage.setItem('adminGreeted', 'true');
+            scope.notifications.push({ message: adminGreeting, type: 'info' });
+            scope.dismissed = false;
+          }
+        }
+
+        loadGreetings();
       }
     }
   }])
